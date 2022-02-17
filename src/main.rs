77991104845d76d23b6a14837811973d99b994dc
Alex Leong <alex@buoyant.io>
2022-02-17 12:31:47 -0800

    Find primary svc from annotation (#31)
    Fixes #7
    
    We refactor the failover controller to eliminate the assumption that there will only ever be one traffic split watched at any given time.  This means removing most of the fields from the context Data object which previously acted as global state.  Instead, we rely on the kube-rs Store caches.
    
    In order to do this, we need access to both the endpoints and trafficsplit caches whenever either an endpoint or trafficsplit is updated.  Ideally this would be possible by adding a "watches" relationshop to say that the trafficsplit controller watches endpoints. Unfortunately, endpoint updates are not stored in this case and therefore we would not have a populated cache of endpoints when doing trafficsplit reconciliation.
    
    Therefore, we replace the endpoint controller with a reflector which does the following on endpoint updates:
    
    * populates the endpoint cache
    * finds any trafficsplits which reference this endpoint, and triggers reconciliation on them
    
    This simplifies the reconciliation logic because the TS reconciler has access to an up-to-date cache of endpoints. 
    
    Signed-off-by: Alex Leong <alex@buoyant.io>
    
    Co-authored-by: Oliver Gould <ver@buoyant.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use futures::{future::TryFutureExt, stream::StreamExt};
 use k8s_openapi::api::core::v1::Endpoints;
 use kube::{
     api::{Api, ListParams, Patch, PatchParams},
-    runtime::controller::{Context, Controller, ReconcilerAction},
+    runtime::{
+        controller::{Context, Controller, ReconcilerAction},
+        reflector::{
+            store::{Store, Writer},
+            ObjectRef,
+        },
+        watcher::Event,
+    },
     Client, CustomResource, ResourceExt,
 };
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
-use tokio::sync::Mutex;
 use tokio::time::Duration;
 use tracing::Instrument;
 use tracing_subscriber::{prelude::*, EnvFilter};
@@ -23,9 +29,6 @@ struct Args {
 
     #[clap(long, default_value = "default")]
     namespace: String,
-
-    #[clap(long)]
-    primary_service: String,
 
     #[clap(long)]
     traffic_split: String,
@@ -49,126 +52,97 @@ struct Backend {
     weight: u32,
 }
 
-struct EndpointsList {
-    ep: Vec<Endpoints>,
-}
-
-struct Data {
+#[derive(Clone)]
+struct Ctx {
     client: Client,
-    ts_name: String,
-    ts_ns: String,
-    svc_primary: String,
-    ts: Arc<Mutex<TrafficSplitSpec>>,
-    ep_list: Arc<Mutex<EndpointsList>>,
+    endpoints: Store<Endpoints>,
+    traffic_splits: Store<TrafficSplit>,
+    ts_ref: ObjectRef<TrafficSplit>,
 }
 
 async fn reconcile_ts(
     ts: Arc<TrafficSplit>,
-    ctx: Context<Data>,
+    ctx: Context<Ctx>,
 ) -> Result<ReconcilerAction, kube::Error> {
-    tracing::debug!(?ts, "traffic split update");
+    tracing::debug!(?ts, "reconciling traffic split");
 
-    let data = ctx.get_ref();
-    let mut ts_state = data.ts.lock().await;
-    ts_state.backends = ts.spec.backends.clone();
-    sync_eps(ts_state.backends.to_vec(), ctx.get_ref()).await?;
-    Ok(ReconcilerAction {
-        requeue_after: Some(Duration::from_secs(300)),
-    })
-}
-
-async fn reconcile_ep(
-    ep: Arc<Endpoints>,
-    ctx: Context<Data>,
-) -> Result<ReconcilerAction, kube::Error> {
-    tracing::debug!(?ep, "endpoint update");
-
-    let data = ctx.get_ref();
-    let mut ts_state = data.ts.lock().await;
-
-    sync_eps(ts_state.backends.to_vec(), data).await?;
-
-    let eps_state = data.ep_list.lock().await;
-
-    let mut failovers_disabled = false;
-    for ep_watched in &eps_state.ep {
-        if ep_watched.name() == data.svc_primary {
-            failovers_disabled = ep_watched.subsets.is_some();
-            break;
+    let namespace = ts.namespace().expect("trafficsplit must be namespaced");
+    let svc_primary = match ts.annotations().get("failover.linkerd.io/primary-service") {
+        Some(name) => name,
+        None => {
+            tracing::info!(
+                %namespace,
+                name = %ts.name(),
+                "trafficsplit is missing the `failover.linkerd.io/primary-service` annotation; skipping"
+            );
+            return Ok(ReconcilerAction {
+                requeue_after: None,
+            });
         }
-    }
+    };
 
-    let mut backends = ts_state.backends.clone();
-    for backend in &mut backends {
-        let mut backend_empty = false;
-        for ep_watched in &eps_state.ep {
-            if ep_watched.name() == backend.service {
-                backend_empty = ep_watched.subsets.is_none();
-                break;
-            }
-        }
-        if backend_empty || (backend.service != data.svc_primary && failovers_disabled) {
-            backend.weight = 0
+    let primary_active = ctx
+        .get_ref()
+        .endpoints
+        .get(&ObjectRef::new(svc_primary).within(&namespace))
+        .map_or(false, |ep| ep.subsets.is_some());
+
+    let mut backends = vec![];
+    for backend in &ts.spec.backends {
+        // Determine if this backend should be active.
+        let active = if &backend.service == svc_primary {
+            // If this service the primary, then use the prior check to determine whether it's active
+            primary_active
         } else {
-            backend.weight = 1
-        }
+            // Otherwise, if the primary is not active, then check the secondary service's state to
+            // determine whether it should be active
+            !primary_active
+                && ctx
+                    .get_ref()
+                    .endpoints
+                    .get(&ObjectRef::new(&backend.service).within(&namespace))
+                    .map_or(false, |ep| ep.subsets.is_some())
+        };
+
+        let mut backend = backend.clone();
+        backend.weight = if active { 1 } else { 0 };
+        backends.push(backend);
     }
 
-    ts_state.backends = backends;
-
-    patch_ts(
-        data.client.clone(),
-        &data.ts_ns,
-        &data.ts_name,
-        ts_state.backends.to_vec(),
-    )
-    .await?;
+    patch_ts(ctx.get_ref(), backends).await?;
 
     Ok(ReconcilerAction {
         requeue_after: Some(Duration::from_secs(300)),
     })
 }
 
-async fn sync_eps(backends: Vec<Backend>, data: &Data) -> Result<(), kube::Error> {
-    let ep_api: Api<Endpoints> = Api::namespaced(data.client.clone(), &data.ts_ns);
-    let mut eps: Vec<Endpoints> = vec![];
-    for backend in backends {
-        let params = ListParams::default().fields(&format!("metadata.name={}", backend.service));
-        if let Some(ep) = ep_api.list(&params).await?.into_iter().next() {
-            eps.push(ep);
-        }
-    }
-
-    let mut ep_list = data.ep_list.lock().await;
-    ep_list.ep = eps;
-    Ok(())
-}
-
-fn error_policy(error: &kube::Error, _ctx: Context<Data>) -> ReconcilerAction {
+fn error_policy(error: &kube::Error, _ctx: Context<Ctx>) -> ReconcilerAction {
     tracing::error!(%error);
     ReconcilerAction {
         requeue_after: Some(Duration::from_secs(1)),
     }
 }
 
-async fn patch_ts(
-    client: Client,
-    ns: &str,
-    name: &str,
-    backends: Vec<Backend>,
-) -> Result<TrafficSplit, kube::Error> {
-    let ts_api: Api<TrafficSplit> = Api::namespaced(client, ns);
+async fn patch_ts(ctx: &Ctx, backends: Vec<Backend>) -> Result<TrafficSplit, kube::Error> {
+    let ns = ctx
+        .ts_ref
+        .namespace
+        .as_ref()
+        .expect("namespace must be set");
+    let ts_api = Api::<TrafficSplit>::namespaced(ctx.client.clone(), ns);
     let ssapply = PatchParams::apply("linkerd_failover_patch");
     let patch = serde_json::json!({
         "apiVersion": "split.smi-spec.io/v1alpha2",
         "kind": "TrafficSplit",
-        "name": name,
+        "name": &ctx.ts_ref.name,
         "spec": {
             "backends": backends
         }
     });
-    tracing::info!(?patch);
-    ts_api.patch(name, &ssapply, &Patch::Merge(patch)).await
+    tracing::debug!(?patch);
+    ts_api
+        .patch(&ctx.ts_ref.name, &ssapply, &Patch::Merge(patch))
+        .await
 }
 
 #[tokio::main]
@@ -177,7 +151,6 @@ async fn main() -> Result<()> {
         log_level,
         namespace,
         traffic_split,
-        primary_service,
     } = Args::parse();
 
     tracing_subscriber::registry()
@@ -192,63 +165,90 @@ async fn main() -> Result<()> {
     );
 
     let client = Client::try_default().await?;
-    let params = ListParams::default().fields(&format!("metadata.name={}", traffic_split));
 
-    let ts_state = Arc::new(Mutex::new(TrafficSplitSpec { backends: vec![] }));
-    let ep_list = Arc::new(Mutex::new(EndpointsList { ep: vec![] }));
+    let ts_controller = {
+        let api = Api::<TrafficSplit>::namespaced(client.clone(), &namespace);
+        let params = ListParams::default().fields(&format!("metadata.name={}", traffic_split));
+        Controller::new(api, params)
+    };
 
-    let ts_api = Api::<TrafficSplit>::namespaced(client.clone(), &namespace);
-    let ts_controller = Controller::new(ts_api, params)
+    let endpoints = Writer::<Endpoints>::new(());
+
+    let ctx = Context::new(Ctx {
+        client: client.clone(),
+        endpoints: endpoints.as_reader(),
+        traffic_splits: ts_controller.store(),
+        ts_ref: ObjectRef::new(&traffic_split).within(&namespace),
+    });
+
+    tokio::spawn({
+        let api = Api::<Endpoints>::namespaced(client.clone(), &namespace);
+        let reflector = kube::runtime::reflector(
+            endpoints,
+            kube::runtime::watcher(api, ListParams::default()),
+        );
+
+        reflector
+            .fold(ctx.clone(), |ctx, ev| async move {
+                match ev {
+                    Ok(ev) => handle_endpoints(ev, &ctx).await,
+                    Err(error) => {
+                        // XXX Are we sure that the reconnect will happen gracefully? Do we need to deal
+                        // with backoff here? Are we going to hit this regularly? (If so, it should be
+                        // debug).
+                        tracing::warn!(%error, "endpoints watch failed");
+                    }
+                }
+                ctx
+            })
+            .instrument(tracing::info_span!("endpoints"))
+    });
+
+    let ts_controller = ts_controller
         .shutdown_on_signal()
-        .run(
-            reconcile_ts,
-            error_policy,
-            Context::new(Data {
-                client: client.clone(),
-                ts_name: traffic_split.clone(),
-                ts_ns: namespace.clone(),
-                svc_primary: primary_service.clone(),
-                ts: ts_state.clone(),
-                ep_list: ep_list.clone(),
-            }),
-        )
+        .run(reconcile_ts, error_policy, ctx)
         .for_each(|res| async move {
-            match res {
-                Ok(o) => tracing::info!("reconciled {:?}", o),
-                Err(error) => tracing::warn!(%error, "reconcile failed"),
+            if let Err(error) = res {
+                tracing::warn!(%error, "reconcile failed");
             }
         })
-        .instrument(tracing::info_span!("trafficsplits"));
-    let ts_controller = tokio::spawn(ts_controller)
-        .unwrap_or_else(|error| panic!("TrafficSplit controller panicked: {}", error));
+        .instrument(tracing::info_span!("trafficsplit"));
 
-    let eps_api = Api::<Endpoints>::namespaced(client.clone(), &namespace);
-    let eps_controller = Controller::new(eps_api, ListParams::default())
-        .shutdown_on_signal()
-        .run(
-            reconcile_ep,
-            error_policy,
-            Context::new(Data {
-                client: client.clone(),
-                ts_name: traffic_split.clone(),
-                ts_ns: namespace.clone(),
-                svc_primary: primary_service.clone(),
-                ts: Arc::clone(&ts_state),
-                ep_list: Arc::clone(&ep_list),
-            }),
-        )
-        .for_each(|res| async move {
-            match res {
-                Ok(o) => tracing::info!("reconciled {:?}", o),
-                Err(error) => tracing::warn!(%error, "reconcile failed"),
-            }
-        })
-        .instrument(tracing::info_span!("endpoints"));
-    let eps_controller = tokio::spawn(eps_controller)
-        .unwrap_or_else(|error| panic!("Endpoints controller panicked: {}", error));
-
-    tokio::join!(ts_controller, eps_controller);
+    tokio::spawn(ts_controller)
+        .unwrap_or_else(|error| panic!("TrafficSplit controller panicked: {}", error))
+        .await;
 
     tracing::info!("controller terminated");
     Ok(())
+}
+
+impl Ctx {
+    fn traffic_split(&self) -> Option<Arc<TrafficSplit>> {
+        self.traffic_splits.get(&self.ts_ref)
+    }
+}
+
+async fn handle_endpoints(ev: Event<Endpoints>, ctx: &Context<Ctx>) {
+    // First, check if the traffic split even exists. If it doesn't, there's no point in
+    // dealing with the endpoint update.
+    let split = match ctx.get_ref().traffic_split() {
+        Some(s) => s,
+        None => return,
+    };
+
+    if let Event::Applied(ep) | Event::Deleted(ep) = ev {
+        // If the endpoint updated is a backend for the traffic split, then we need to
+        // reconcile.
+
+        // On restart, we could potentially have missed a delete event, so always
+        // reconcile.
+        if !split.spec.backends.iter().any(|b| b.service == ep.name()) {
+            // No reconcile needed.
+            return;
+        }
+    }
+
+    if let Err(error) = reconcile_ts(split.clone(), ctx.clone()).await {
+        tracing::warn!(%error, "reconcile failed");
+    }
 }
