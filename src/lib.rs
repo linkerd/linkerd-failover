9a3612d5bb60c4117f7113d4c56ca5b908ee25de
Oliver Gould <ver@buoyant.io>
2022-03-01 23:51:56 +0000

    Add test helper functions to reduce type boilerplate
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,66 @@ mod tests {
         )
     }
 
+    fn endpoint_ready(name: impl Into<String>, ip: impl Into<String>) -> Endpoints {
+        Endpoints {
+            metadata: kube::core::ObjectMeta {
+                name: Some(name.into()),
+                namespace: Some("default".to_owned()),
+                ..Default::default()
+            },
+            subsets: Some(vec![EndpointSubset {
+                addresses: Some(vec![EndpointAddress {
+                    ip: ip.into(),
+                    ..Default::default()
+                }]),
+                ..Default::default()
+            }]),
+        }
+    }
+
+    fn endpoint_not_ready(name: impl Into<String>, ip: impl Into<String>) -> Endpoints {
+        Endpoints {
+            metadata: kube::core::ObjectMeta {
+                name: Some(name.into()),
+                namespace: Some("default".to_owned()),
+                ..Default::default()
+            },
+            subsets: Some(vec![EndpointSubset {
+                not_ready_addresses: Some(vec![EndpointAddress {
+                    ip: ip.into(),
+                    ..Default::default()
+                }]),
+                ..Default::default()
+            }]),
+        }
+    }
+
+    fn traffic_split(
+        name: impl Into<String>,
+        primary: impl Into<String>,
+        backends: Vec<traffic_split::Backend>,
+    ) -> TrafficSplit {
+        TrafficSplit {
+            metadata: kube::core::ObjectMeta {
+                name: Some(name.into()),
+                namespace: Some("default".to_owned()),
+                annotations: Some(
+                    Some((
+                        "failover.linkerd.io/primary-service".to_owned(),
+                        primary.into(),
+                    ))
+                    .into_iter()
+                    .collect(),
+                ),
+                ..Default::default()
+            },
+            spec: traffic_split::TrafficSplitSpec {
+                backends,
+                ..Default::default()
+            },
+        }
+    }
+
     /// Given a traffic split with 3 backends, all of which have ready addresses, the traffic split
     /// is patched to use that specified in the primary-service annotation.
     #[tokio::test]
@@ -91,91 +151,39 @@ mod tests {
         let (ctx, mut endpoints, mut trafficsplit, mut patches) = mk_ctx(10);
 
         let restart_eps = Event::Restarted(vec![
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("primary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.13".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("secondary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.14".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("tertiary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.15".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
+            endpoint_ready("primary", "10.11.12.13"),
+            endpoint_ready("secondary", "10.11.12.14"),
+            endpoint_ready("tertiary", "10.11.12.15"),
         ]);
         endpoints.apply_watcher_event(&restart_eps);
         endpoints::handle(restart_eps, &ctx).await;
         assert_pending!(patches.poll_next());
 
-        let restart_ts = Event::Restarted(vec![TrafficSplit {
-            metadata: kube::core::ObjectMeta {
-                name: Some("ts0".to_owned()),
-                namespace: Some("ns0".to_owned()),
-                annotations: Some(
-                    Some((
-                        "failover.linkerd.io/primary-service".to_owned(),
-                        "primary".to_owned(),
-                    ))
-                    .into_iter()
-                    .collect(),
-                ),
-                ..Default::default()
-            },
-            spec: traffic_split::TrafficSplitSpec {
-                backends: vec![
-                    traffic_split::Backend {
-                        service: "primary".to_owned(),
-                        weight: 1,
-                    },
-                    traffic_split::Backend {
-                        service: "secondary".to_owned(),
-                        weight: 1,
-                    },
-                    traffic_split::Backend {
-                        service: "tertiary".to_owned(),
-                        weight: 1,
-                    },
-                ],
-            },
-        }]);
+        let restart_ts = Event::Restarted(vec![traffic_split(
+            "ts0",
+            "primary",
+            vec![
+                traffic_split::Backend {
+                    service: "primary".to_owned(),
+                    weight: 1,
+                },
+                traffic_split::Backend {
+                    service: "secondary".to_owned(),
+                    weight: 1,
+                },
+                traffic_split::Backend {
+                    service: "tertiary".to_owned(),
+                    weight: 1,
+                },
+            ],
+        )]);
         trafficsplit.apply_watcher_event(&restart_ts);
         traffic_split::handle(restart_ts, &ctx).await;
 
         assert_ready_eq!(
             patches.poll_next(),
             Some(traffic_split::FailoverUpdate {
-                target: ObjectRef::new("ts0").within("ns0"),
+                target: ObjectRef::new("ts0").within("default"),
                 backends: vec![
                     traffic_split::Backend {
                         service: "primary".to_owned(),
@@ -202,91 +210,39 @@ mod tests {
         let (ctx, mut endpoints, mut trafficsplit, mut patches) = mk_ctx(10);
 
         let restart_eps = Event::Restarted(vec![
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("primary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    not_ready_addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.13".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("secondary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.14".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("tertiary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.15".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
+            endpoint_not_ready("primary", "10.11.12.13"),
+            endpoint_ready("secondary", "10.11.12.14"),
+            endpoint_ready("tertiary", "10.11.12.15"),
         ]);
         endpoints.apply_watcher_event(&restart_eps);
         endpoints::handle(restart_eps, &ctx).await;
         assert_pending!(patches.poll_next());
 
-        let restart_ts = Event::Restarted(vec![TrafficSplit {
-            metadata: kube::core::ObjectMeta {
-                name: Some("ts0".to_owned()),
-                namespace: Some("ns0".to_owned()),
-                annotations: Some(
-                    Some((
-                        "failover.linkerd.io/primary-service".to_owned(),
-                        "primary".to_owned(),
-                    ))
-                    .into_iter()
-                    .collect(),
-                ),
-                ..Default::default()
-            },
-            spec: traffic_split::TrafficSplitSpec {
-                backends: vec![
-                    traffic_split::Backend {
-                        service: "primary".to_owned(),
-                        weight: 1,
-                    },
-                    traffic_split::Backend {
-                        service: "secondary".to_owned(),
-                        weight: 0,
-                    },
-                    traffic_split::Backend {
-                        service: "tertiary".to_owned(),
-                        weight: 0,
-                    },
-                ],
-            },
-        }]);
+        let restart_ts = Event::Restarted(vec![traffic_split(
+            "ts0",
+            "primary",
+            vec![
+                traffic_split::Backend {
+                    service: "primary".to_owned(),
+                    weight: 1,
+                },
+                traffic_split::Backend {
+                    service: "secondary".to_owned(),
+                    weight: 0,
+                },
+                traffic_split::Backend {
+                    service: "tertiary".to_owned(),
+                    weight: 0,
+                },
+            ],
+        )]);
         trafficsplit.apply_watcher_event(&restart_ts);
         traffic_split::handle(restart_ts, &ctx).await;
 
         assert_ready_eq!(
             patches.poll_next(),
             Some(traffic_split::FailoverUpdate {
-                target: ObjectRef::new("ts0").within("ns0"),
+                target: ObjectRef::new("ts0").within("default"),
                 backends: vec![
                     traffic_split::Backend {
                         service: "primary".to_owned(),
@@ -312,84 +268,32 @@ mod tests {
         let (ctx, mut endpoints, mut trafficsplit, mut patches) = mk_ctx(10);
 
         let restart_eps = Event::Restarted(vec![
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("primary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.13".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("secondary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.14".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
-            Endpoints {
-                metadata: kube::core::ObjectMeta {
-                    name: Some("tertiary".to_owned()),
-                    namespace: Some("ns0".to_owned()),
-                    ..Default::default()
-                },
-                subsets: Some(vec![EndpointSubset {
-                    addresses: Some(vec![EndpointAddress {
-                        ip: "10.11.12.15".to_owned(),
-                        ..Default::default()
-                    }]),
-                    ..Default::default()
-                }]),
-            },
+            endpoint_ready("primary", "10.11.12.13"),
+            endpoint_ready("secondary", "10.11.12.14"),
+            endpoint_ready("tertiary", "10.11.12.15"),
         ]);
         endpoints.apply_watcher_event(&restart_eps);
         endpoints::handle(restart_eps, &ctx).await;
         assert_pending!(patches.poll_next());
 
-        let restart_ts = Event::Restarted(vec![TrafficSplit {
-            metadata: kube::core::ObjectMeta {
-                name: Some("ts0".to_owned()),
-                namespace: Some("ns0".to_owned()),
-                annotations: Some(
-                    Some((
-                        "failover.linkerd.io/primary-service".to_owned(),
-                        "primary".to_owned(),
-                    ))
-                    .into_iter()
-                    .collect(),
-                ),
-                ..Default::default()
-            },
-            spec: traffic_split::TrafficSplitSpec {
-                backends: vec![
-                    traffic_split::Backend {
-                        service: "primary".to_owned(),
-                        weight: 1,
-                    },
-                    traffic_split::Backend {
-                        service: "secondary".to_owned(),
-                        weight: 0,
-                    },
-                    traffic_split::Backend {
-                        service: "tertiary".to_owned(),
-                        weight: 0,
-                    },
-                ],
-            },
-        }]);
+        let restart_ts = Event::Restarted(vec![traffic_split(
+            "ts0",
+            "primary",
+            vec![
+                traffic_split::Backend {
+                    service: "primary".to_owned(),
+                    weight: 1,
+                },
+                traffic_split::Backend {
+                    service: "secondary".to_owned(),
+                    weight: 0,
+                },
+                traffic_split::Backend {
+                    service: "tertiary".to_owned(),
+                    weight: 0,
+                },
+            ],
+        )]);
         trafficsplit.apply_watcher_event(&restart_ts);
         traffic_split::handle(restart_ts, &ctx).await;
 
