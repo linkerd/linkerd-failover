1a8af590cb2ccc1e2c16523d8c46b0e95305d1de
Alex Leong <alex@buoyant.io>
2022-03-10 01:38:46 +0000

    Add status command
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- cli/src/status.rs
+++ cli/src/status.rs
@@ -1,5 +1,8 @@
-use kube::Client;
+use anyhow::Result;
+use kube::{api::ListParams, Api, Client, ResourceExt};
+use linkerd_failover_controller::TrafficSplit;
 use serde::Serialize;
+use std::fmt::Display;
 
 #[derive(Serialize)]
 pub struct TrafficSplitStatus {
@@ -16,15 +19,79 @@ enum FailoverStatus {
     Fallback,
 }
 
-pub async fn status(client: Client) -> Vec<TrafficSplitStatus> {
-    unimplemented!()
+impl Display for FailoverStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Self::Primary => std::fmt::Display::fmt("Primary", f)?,
+            Self::Fallback => std::fmt::Display::fmt("Fallback", f)?,
+        };
+        Ok(())
+    }
 }
 
-pub fn print_status(results: Vec<TrafficSplitStatus>) -> bool {
-    unimplemented!()
+pub async fn status(client: Client, label_selector: &str) -> Result<Vec<TrafficSplitStatus>> {
+    let api = Api::<TrafficSplit>::all(client);
+    let list_params = ListParams::default().labels(label_selector);
+    let traffic_splits = api.list(&list_params).await?;
+    let statuses = traffic_splits
+        .items
+        .into_iter()
+        .flat_map(|ts| {
+            let primary = ts
+                .metadata
+                .annotations
+                .as_ref()
+                .and_then(|annotations| annotations.get("failover.linkerd.io/primary-service"));
+            primary.map(|primary| {
+                let active_backends = active_backends(&ts);
+                let status = if active_backends.contains(primary) {
+                    FailoverStatus::Primary
+                } else {
+                    FailoverStatus::Fallback
+                };
+                TrafficSplitStatus {
+                    namespace: ts.namespace().expect("TrafficSplits must be namespaced"),
+                    name: ts.name(),
+                    status,
+                    services: active_backends,
+                }
+            })
+        })
+        .collect();
+    Ok(statuses)
 }
 
+pub fn print_status(results: Vec<TrafficSplitStatus>) {
+    println!(
+        "{:15}\t{:15}\t{:10}\tACTIVE BACKENDS",
+        "NAMESPACE", "TRAFFIC_SPLIT", "STATUS"
+    );
+    for result in results.iter() {
+        println!(
+            "{:15}\t{:15}\t{:10}\t{}",
+            result.namespace,
+            result.name,
+            result.status,
+            result.services.join(", ")
+        );
+    }
+}
 
-pub fn json_print_status(results: Vec<TrafficSplitStatus>) -> bool {
-    unimplemented!()
+pub fn json_print_status(results: Vec<TrafficSplitStatus>) {
+    serde_json::to_writer_pretty(std::io::stdout(), &results).expect("serialization failed");
+    println!();
+}
+
+fn active_backends(ts: &TrafficSplit) -> Vec<String> {
+    ts.spec
+        .backends
+        .iter()
+        .filter_map(|backend| {
+            if backend.weight > 0 {
+                Some(backend.service.clone())
+            } else {
+                None
+            }
+        })
+        .collect()
 }
